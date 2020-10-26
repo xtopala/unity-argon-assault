a550a3da6f3f3f98858ee456f0c047f80920ed77
xtopala <xtopala@gmail.com>
2020-10-26 20:50:23 +0100

    added movement

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -3,6 +3,10 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
+    [Tooltip("In ms^-1")][SerializeField] float speed = 20f;
+    [Tooltip("In m")][SerializeField] float xRange = 5f;
+    [Tooltip("In m")][SerializeField] float yRange = 3f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -12,6 +16,19 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        
+        float xOffset = xThrow * speed * Time.deltaTime;
+        float yOffset = yThrow * speed * Time.deltaTime;
+        
+        float rawNewXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(rawNewXPos, -xRange, xRange);
+
+        float rawNewYPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(rawNewYPos, -yRange, yRange);
+
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
     }
 }

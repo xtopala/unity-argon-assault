67c3f160526668bb8d717b5870cff791c523a82d
xtopala <xtopala@gmail.com>
2020-10-26 12:26:25 +0100

    Added cross platform input

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke(nameof(LoadFirstScene), 2f);
+    }
+
+    void LoadFirstScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}

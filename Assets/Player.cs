3072f0fba49a9338f5f0bcffcc900bbff193de27
xtopala <xtopala@gmail.com>
2020-10-28 13:06:51 +0100

    Finished tweak and tune

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
@@ -6,6 +7,13 @@ public class Player : MonoBehaviour
     [Tooltip("In ms^-1")][SerializeField] float speed = 20f;
     [Tooltip("In m")][SerializeField] float xRange = 5f;
     [Tooltip("In m")][SerializeField] float yRange = 3f;
+
+    [SerializeField] float positionPitchFactor = 5f;
+    [SerializeField] float controlPitchFactor = -20f;
+    [SerializeField] float positionYawFactor = 5f;
+    [SerializeField] float controlRollFactor = -20f;
+
+    float xThrow, yThrow;
 
     // Start is called before the first frame update
     void Start()
@@ -16,12 +24,31 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
-        
+        ProcessTranslation();
+        ProcessRotation();
+    }
+
+    private void ProcessRotation()
+    {
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchDueToControlPosition = yThrow * controlPitchFactor;
+        float pitch = pitchDueToPosition + pitchDueToControlPosition;
+
+        float yaw = transform.localPosition.x * positionYawFactor;
+
+        float roll = xThrow * controlRollFactor;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    private void ProcessTranslation()
+    {
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+
         float xOffset = xThrow * speed * Time.deltaTime;
         float yOffset = yThrow * speed * Time.deltaTime;
-        
+
         float rawNewXPos = transform.localPosition.x + xOffset;
         float clampedXPos = Mathf.Clamp(rawNewXPos, -xRange, xRange);
 
